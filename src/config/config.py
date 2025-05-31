e29d258785ff88cf7f95bbcbda832396d63d3bc4
Nguyen Long Vu <167902382+ZACZ1NH0@users.noreply.github.com>
2025-05-31 15:00:16 +0700

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -1,1 +1,7 @@
-
+DATA_PATH = "data/"
+PROCESSED_PATH = DATA_PATH + "processed/person1/"
+EMBEDDINGS_PATH = DATA_PATH + "embeddings/person1_embeddings.npz"
+VIDEO_PATH = DATA_PATH + "videos/input_video.mp4"
+RESULTS_PATH = DATA_PATH + "results/"
+THRESHOLD = 0.6  # Ngưỡng so sánh embeddings
+VIDEO_RESOLUTION = (640, 480)
