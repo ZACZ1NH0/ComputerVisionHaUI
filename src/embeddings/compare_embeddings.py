ac7223c00e4a5d09b41988cbde7c124ce9919d23
NamCaoNg <caonamk96@gmail.com>
2025-06-10 15:02:21 +0700

    Push lai

--- src/embeddings/compare_embeddings.py
+++ src/embeddings/compare_embeddings.py
@@ -1,2 +1,20 @@
+import numpy as np
+from scipy.spatial.distance import cosine
+from config import THRESHOLD
 
+def compare_embeddings(input_embedding, stored_embeddings, stored_labels, threshold=THRESHOLD):
+
+    min_dist = float("inf")
+    best_match = "Unknown"
+
+    for emb, label in zip(stored_embeddings, stored_labels):
+        dist = cosine(input_embedding, emb)
+        if dist < min_dist:
+            min_dist = dist
+            best_match = label
+
+    if min_dist > threshold:
+        best_match = "Unknown"
+
+    return best_match, min_dist
 
