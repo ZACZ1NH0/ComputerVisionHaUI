c1a320c2a2a952f7c549abea77804cfc74157b3b
TaTuan-no1 <tqt060404@gmail.com>
2025-06-10 20:16:59 +0700

    fds

--- src/config/config.py
+++ src/config/config.py
@@ -1,7 +1,19 @@
-DATA_PATH = "data/"
-PROCESSED_PATH = DATA_PATH + "processed/person1/"
-EMBEDDINGS_PATH = DATA_PATH + "embeddings/person1_embeddings.npz"
-VIDEO_PATH = DATA_PATH + "videos/input_video.mp4"
-RESULTS_PATH = DATA_PATH + "results/"
-THRESHOLD = 0.6  # Ngưỡng so sánh embeddings
+# DATA_PATH = "data/"
+# PROCESSED_PATH = DATA_PATH + "processed/"
+# EMBEDDINGS_PATH = DATA_PATH + "embeddings/embeddings.pkl"
+# TEST = DATA_PATH +  "raw/"
+# VIDEO_PATH = DATA_PATH + "videos/input_video.mp4"
+# RESULTS_PATH = DATA_PATH + "results/"
+# THRESHOLD = 0.6  # Ngưỡng so sánh embeddings
+# VIDEO_RESOLUTION = (640, 480)
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+DATA_PATH = os.path.join(BASE_DIR, "data")
+PROCESSED_PATH = os.path.join(DATA_PATH, "processed")
+EMBEDDINGS_PATH = os.path.join(DATA_PATH, "embeddings", "all_embeddings.pkl")
+VIDEO_PATH = os.path.join(DATA_PATH, "videos", "input_video.mp4")
+RESULTS_PATH = os.path.join(DATA_PATH, "results")
+TEST = os.path.join(DATA_PATH, "raw")
+THRESHOLD = 0.6
 VIDEO_RESOLUTION = (640, 480)
